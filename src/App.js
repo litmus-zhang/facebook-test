40cd001924dddccade54265f60f152feccd6cacd
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-04-16 09:56:07 +0100

    --first commit
    All components
    showing, left with the dropdown funtionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {ReactComponent as Add} from './icons/Add Circle.svg'
+import {ReactComponent as Alert} from './icons/Alert.svg'
+import {ReactComponent as Bookmark} from './icons/Bookmark Add.svg'
+import {ReactComponent as Briefcase} from './icons/Briefcase Medical.svg'
+import { ReactComponent as Camera } from './icons/Camera.svg'
+import React ,{useState} from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+      <NavItem icon={<Add/>} />
+      <NavItem icon={<Alert/>} />
+      <NavItem icon={<Bookmark />} />
+      <NavItem icon={<Briefcase />}>
+<DropdownMenu/>
+        </NavItem>
+   </Navbar>
   );
+}
+function DropdownMenu()
+{
+  function DropdownItem(props)
+  {
+    return (
+      <a className="meu-item" href="#">
+        <span className='icon-button'>{props.leftIcon }</span>
+        {props.children}
+        <span className='icon-right'>{props.rightIcon }</span>
+      
+      </a>
+    )
+  }
+  <div className='dropdown'>
+    <DropdownItem>My Profile</DropdownItem>
+    <DropdownItem>
+      leftIcon={<Camera />}
+      rightIcon={<Bookmark />}
+
+
+    </DropdownItem>
+  </div>
+}
+function Navbar(props)
+{
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  )
+}
+function NavItem(props)
+{
+  const [open, setOpen] = useState(false);
+  return (
+    <li className="nav-item">
+      <a className="icon-button" href="#" onClick={() => setOpen(!open)}>{props.icon}</a>
+      {open && props.children}
+      </li>
+  )
 }
 
 export default App;
